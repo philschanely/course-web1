c53a98e54dd59dd3e0c1d4f3f6e7b24d4143685e
Phil Schanely <philschanely@gmail.com>
2018-04-04 20:51:49 -0400

    Update build paths

--- _script/deploy.sh
+++ _script/deploy.sh
@@ -3,11 +3,11 @@
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
     ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-    rm -rf web1/site
-    mkdir web1/site
+    rm -rf web1
+    mkdir web1
     quit
 EOF
 
     cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /web1/site .
+    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /web1 .
 fi
