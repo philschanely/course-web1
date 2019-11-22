455dd7e673b6af4a3ea0b3e8e4d3064c6fa0f1e2
Phil Schanely <phil.schanely@dockyard.com>
2019-11-22 11:20:21 -0500

    Update font loading method

--- src/components/layout.js
+++ src/components/layout.js
@@ -23,7 +23,7 @@ const Layout = ({ activePath, children, title }) => {
   return (
     <>
       <Helmet>
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-zrnmn8R8KkWl12rAZFt4yKjxplaDaT7/EUkKm7AovijfrQItFWR7O/JJn4DAa/gx" crossorigin="anonymous" />
+        <script src="https://kit.fontawesome.com/848ac60b9f.js" crossorigin="anonymous"></script>
         <link type="text/css" rel="stylesheet" href={`${process.env.GATSBY_TOOLKIT_URL}/assets/toolkit/styles/toolkit.css`} />
       </Helmet>
       <Header activePath={activePath} pages={data.site.siteMetadata.pages} title={data.site.siteMetadata.title} />
